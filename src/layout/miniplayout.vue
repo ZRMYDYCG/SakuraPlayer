63b952d7ab0041a524ee2170c45042dd9f04daaa
ZRMYDYCG <547471919@qq.com>
2024-10-20 17:02:34 +0800

    feat: 底部安全区域

--- src/layout/miniplayout.vue
+++ src/layout/miniplayout.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: ZRMYDYCG
+ * @Date: 2024-10
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2024-10
+ * @Description: 
+-->
 <template>
     <div class="min-play-out" :class="{ padding_out: currentSong.id }">
         <slot></slot>
@@ -31,7 +38,9 @@ const { currentSong } = storeToRefs(playerStore)
     width: 100%;
     transition: all 0.4s ease;
     z-index: 10;
+    /* 这个属性是为了适配 iOS 11 之前的版本。在这些版本中，constant() 函数用于获取安全区域的底部插入值。 */
     padding-bottom: constant(safe-area-inset-bottom);
+    /* 这个属性是用于现代设备（尤其是支持安全区域的设备，如 iPhone X 及更新版本）来处理界面安全区域的 */
     padding-bottom: env(safe-area-inset-bottom);
     background: var(--my-back-color-white);
 }
