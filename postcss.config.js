71088f34bd25d827910eb1a5a0ba4590ff1b26fb
ZRMYDYCG <547471919@qq.com>
2024-10-20 12:47:53 +0800

    feat: 适配 vant

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+/*
+ * @Author: ZRMYDYCG
+ * @Date: 2024-10
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2024-10
+ * @Description: 
+ */
+module.exports = {
+	plugins: {
+		'postcss-pxtorem': {
+			rootValue({
+				file
+			}) {
+				return file.indexOf('vant') !== -1 ? 37.5 : 75;
+			},
+			propList: ['*'],
+		},
+	},
 }
--- src/api/modules/album.ts
+++ src/api/modules/album.ts
@@ -8,24 +8,28 @@ export const reqAlbumList = (params: AlbumListParams) :Promise<AxiosResponse> =>
 	method: 'GET',
 	params
 })
+
 // 获取数字专辑 
 export const reqDigitAlbum = (params: DigitAlbumParams) :Promise<AxiosResponse> => request({
 	url: '/album/songsaleboard',
 	method: 'GET',
 	params
 })
+
 // 数字专辑详情
 export const reqAlbumDetail = (params: AlbumDetailParams) :Promise<AxiosResponse> => request({
 	url: '/album/detail',
 	method: 'GET',
 	params
 })
+
 // 专辑详情 /digitalAlbum/detail
 export const reqDigitalAlbum = (params: AlbumDetailParams) :Promise<AxiosResponse> => request({
 	url: '/digitalAlbum/detail',
 	method: 'GET',
 	params
 })
+
 // 获取专辑内容
 export const reqAlbum = (params :AlbumDetailParams) :Promise<AxiosResponse> => request({
 	url: '/album',
