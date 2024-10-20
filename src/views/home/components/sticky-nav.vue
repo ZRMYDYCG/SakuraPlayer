238982b92d22bde59c1b5dd7cbd7c37baf79115d
ZRMYDYCG <547471919@qq.com>
2024-10-20 17:51:59 +0800

    peft: 优化home页面粘性导航组件

---
+++ src/views/home/components/sticky-nav.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import $bus from '@/utils/eventBus'
+import { useRouter } from 'vue-router'
+import { reqSearchDefault } from '@/api/modules/search'
+
+const router = useRouter()
+const showKeyword = ref<string>('')
+
+function goSearch(): void {
+    router.push({
+        path: '/search'
+    })
+}
+
+function openMenu(): void {
+    $bus.emit('opne_menu')
+}
+
+function getDefaultWord(): void {
+    reqSearchDefault().then((res) => {
+        showKeyword.value = res.data.data.showKeyword
+    })
+}
+getDefaultWord()
+</script>
+
+<template>
+    <van-sticky>
+        <div class="nav">
+            <div class="flex_box_center_column" @click="openMenu">
+                <i class="iconfont icon-caidan"></i>
+            </div>
+            <div class="search_wrapper" @click.stop="goSearch">
+                <div class="flex_box_center_column">
+                    <i class="iconfont icon-sousuo"></i>
+                </div>
+                <div class="search_text flex_box_center_column">{{ showKeyword }}</div>
+            </div>
+            <div class="flex_box_center_column">
+                <i class="iconfont icon-yuyin"></i>
+            </div>
+        </div>
+    </van-sticky>
+</template>
+
+<style lang="scss" scoped>
+.nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 20px;
+    height: 88px;
+    background: var(--my-gray-1);
+    .iconfont {
+        color: var(--my-text-color-black);
+    }
+    .search_wrapper {
+        height: 60px;
+        background: var(--my-search-back);
+        flex: 1;
+        margin: 0 40px;
+        display: flex;
+        border-radius: 30px;
+        display: flex;
+        justify-content: center;
+        color: var(--my-text-color-black);
+        .icon-sousuo {
+            padding-top: 4px;
+        }
+
+        .search_text {
+            margin-left: 10px;
+            font-size: 24px;
+        }
+    }
+}
+
+.icon-yuyin,
+.icon-caidan {
+    font-size: 40px;
+    font-weight: 600;
+}
+</style>
