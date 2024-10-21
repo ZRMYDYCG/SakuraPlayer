42a118983121c1d6e8017b539bdc4c1f2056a564
ZRMYDYCG <547471919@qq.com>
2024-10-21 19:39:03 +0800

    feat: 确定键盘是否按下某个按键

---
+++ src/hooks/useKeyPress.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: ZRMYDYCG
+ * @Date: 2024-10
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2024-10
+ * @Description: 确定键盘是否按下某个按键
+ */
+import { ref } from 'vue'
+
+export const useKeyPress = (targetKey: string) => {
+    const keyPressed = ref(false)
+    window.addEventListener('keydown', (event) => {
+        if (event.key === targetKey) {
+            keyPressed.value = true
+        }
+    })
+    window.addEventListener('keyup', (event) => {
+        if (event.key === targetKey) {
+            keyPressed.value = false
+        }
+    })
+    return keyPressed
+}
