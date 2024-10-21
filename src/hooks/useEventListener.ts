88afc064175744b0e05104a2140a089c881687cd
ZRMYDYCG <547471919@qq.com>
2024-10-21 19:31:07 +0800

    feat: 事件监听hook

---
+++ src/hooks/useEventListener.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: ZRMYDYCG
+ * @Date: 2024-10
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2024-10
+ * @Description: 事件监听
+ */
+import { onMounted, onUnmounted } from "vue"
+
+export const useEventListener = (event: string, handler: (event: Event) => void, options?: boolean | AddEventListenerOptions) => {
+    onMounted(() => {
+        window.addEventListener(event, handler, options)
+    })
+    onUnmounted(() => {
+        window.removeEventListener(event, handler, options)
+    })
+}
