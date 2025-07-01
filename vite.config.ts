1615403317ce31e1d5c73453d18bf199918632c3
ZRMYDYCG <547471919@qq.com>
2025-07-01 18:49:34 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -18,18 +18,20 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
+            // 根据需要添加更多别名
+            // '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
         }
     },
     server: {
-        open: true,
-        hmr: true,
-        host: '0.0.0.0',
+        open: true, // 启动时自动打开浏览器
+        hmr: true, // 热更新
+        host: '0.0.0.0', // 支持真机调试
         port: 3030,
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
+                target: 'http://localhost:3000', // 代理地址
+                changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+                rewrite: (path) => path.replace(/^\/api/, '') // 重写路径
             }
         }
     },
@@ -37,7 +39,13 @@ export default defineConfig({
         outDir: 'dist', // 输出目录
         assetsDir: 'static' // 生成的静态资源目录
     },
+    define: {
+        'process.env': {} // 定义环境变量
+    },
     css: {
-        postcss: {}
+        postcss: {
+ 
+        }
     }
 })
+
